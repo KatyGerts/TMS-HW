a27589c2a92e74894bfb66b91ca7c4c2f91520d9
KatyGerts <Katya.gertsmanovich@gmai.com>
2022-10-15 20:14:36 +0300

    Add hw4-1,hw4

---
+++ hw4-1.ts
@@ -0,0 +1,3 @@
+let s1 = 'я учу typescript!';
+let arr = s1.split(" ");
+console.log(arr);
---
+++ hw4.ts
@@ -0,0 +1,6 @@
+let s = 'я учу typescript!';
+let u = 'учу';
+if (s.includes(u))
+    console.log(s.indexOf(u));
+else 
+    console.log('Данной подстроки нет');
